60bfd467f64df11780feb1507becccca27a3801c
Annu <annusinghraj1998@gmail.com>
2022-08-12 05:41:36 +0530

    Created Address book app development spring project

---
+++ src/main/java/com/bridgelabz/addressbookprogram/AddressBookProgramApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.addressbookprogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressBookProgramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AddressBookProgramApplication.class, args);
+    }
+
+}
