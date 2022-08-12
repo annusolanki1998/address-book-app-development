1f5f80de4d47dfb12cae3509e9050178959eb0a0
Annu <annusinghraj1998@gmail.com>
2022-08-12 07:27:08 +0530

    ADDED: Create address book dto and model class and add one jpa dependency

---
+++ src/main/java/com/bridgelabz/addressbookprogram/model/AddressBookModel.java
@@ -0,0 +1,40 @@
+package com.bridgelabz.addressbookprogram.model;
+
+import com.bridgelabz.addressbookprogram.dto.AddressBookDTO;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+
+@Data
+@Entity
+@Table(name = "AddressBook")
+public class AddressBookModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String state;
+    private int zip;
+    private long phoneNumber;
+    private LocalDateTime registerDate;
+    private LocalDateTime updatedDate;
+
+    public AddressBookModel(AddressBookDTO addressBookDTO) {
+        this.firstName = addressBookDTO.getFirstName();
+        this.lastName = addressBookDTO.getLastName();
+        this.address = addressBookDTO.getAddress();
+        this.city = addressBookDTO.getCity();
+        this.state = addressBookDTO.getState();
+        this.phoneNumber = addressBookDTO.getPhoneNumber();
+        this.zip = addressBookDTO.getZip();
+    }
+
+    public AddressBookModel() {
+
+    }
+}
